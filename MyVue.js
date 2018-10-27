e0415b1e761083df6d00b4121acc311d4d89e72a
ChenYongYang <1214137301@qq.com>
2018-10-27 16:21:04 +0800

    将data上的数据代理到实例上

--- MyVue.js
+++ MyVue.js
@@ -1,7 +1,31 @@
 function MyVue(data, el, exp){
+    let self = this;
     this.data = data;
+
+    Object.keys(data).forEach(key => {
+        self.proxyKey(key)
+    })
+
     observe(data);
-    el.innerHTML = this.data[exp];
-    new Watcher(this, exp, value => el.innerHTML = value);
+    el.innerHTML = this.data[exp]; // 初始化模板数据
+    new Watcher(this, exp, value => {
+        el.innerHTML = value
+    });
     return this;
+}
+
+MyVue.prototype = {
+    proxyKey(key){
+        let self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function proxyGetter() {
+                return self.data[key];
+            },
+            set: function(newVal) {
+                self.data[key] = newVal;
+            }
+        })
+    }
 }
